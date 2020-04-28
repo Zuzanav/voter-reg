63118b1e4c276aa885a08bb67ab046822415e934
Zuzanav <zuzanavolny@gmail.com>
2020-04-28 14:23:32 -0700

    retrieving top ten for voter registration numbers

--- voter-data/app.js
+++ voter-data/app.js
@@ -9,13 +9,14 @@ function loopForData(all_data) {
   var total_pop = [];
   var unsorted_total_pop = [];
   var counties = [];
+  var total_reg = [];
 
   // Loops through all the data pulled from API and stores it into individual variables
   for (i=0; i < all_data.length; i++) { //-----------------------------------------------
 
     var county_name = all_data[i].county; // COUNTY NAME (ONLY IN CALIFORNIA)
     var wholePop = all_data[i].cvap_net; // ENTIRE POPULATION PER COUNTY
-    var totalReg = all_data[i].total_registered_voters; // TOTAL REGISTERED TO VOTE PER COUNTY
+    var total_reg_num = all_data[i].total_registered_voters; // TOTAL REGISTERED TO VOTE PER COUNTY
     var age18_25 = all_data[i].age_18_to_25; // OF AGE GROUP REGISTERED TO VOTE PER COUNTY
     var age26_35 = all_data[i].age_26_to_35; // "
     var age36_45 = all_data[i].age_36_to_45; // "
@@ -27,6 +28,8 @@ function loopForData(all_data) {
     total_pop.push(wholePop);
     unsorted_total_pop.push(wholePop);
     counties.push(county_name);
+    total_reg.push(total_reg_num);
+
 
   } // END OF FOR LOOP --------------------------------------------------------------------
 
@@ -37,23 +40,36 @@ function loopForData(all_data) {
       });
 
   // Take counties with highest population 
-  // Loop through array with sorted populations for each county, and print only first 10 (highest)
+  // Loop through array with sorted populations for each county, and save only first 10 (highest)
   var topTenPop = [];
+  var topTenIndex = [];
   var topTenCounties = [];
+  var topTenReg = [];
 
 
   for (j = 0; j < 10; j++) {
+    // push TOP 10 - TOTAL POPULATION numbers to array
     topTenPop.push(new_total_pop[j]);
-    topTenCounties.push((unsorted_total_pop.indexOf(new_total_pop[j])));
 
-    console.log(counties[topTenCounties[j]]);
+    // push TOP TEN - INDEX NUMBERS to array
+    topTenIndex.push((unsorted_total_pop.indexOf(new_total_pop[j])));
+
+    // push TOP 10 - COUNTY NAMES to array
+    topTenCounties.push((counties[topTenIndex[j]]))
+
+    // push TOP 10 - REGISTERED VOTERS numbers to array
+    topTenReg.push((total_reg[topTenIndex[j]]))
+
+
+
 
 
   };
 
 
-
   console.log(topTenCounties);
+  console.log(topTenReg);
+
 
 
 
