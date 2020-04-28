059d8724be024b14da7c9e2aad7549128bc4668a
Zuzanav <zuzanavolny@gmail.com>
2020-04-28 15:03:42 -0700

    successfully added top 10 of all needed data into arrays

--- voter-data/app.js
+++ voter-data/app.js
@@ -10,9 +10,16 @@ function loopForData(all_data) {
   var unsorted_total_pop = [];
   var counties = [];
   var total_reg = [];
+  var group_1 = [];
+  var group_2 = [];
+  var group_3 = [];
+  var group_4 = [];
+  var group_5 = [];
+  var group_6 = [];
 
+  // BEGIN FOR LOOP ------------------------------------------------------------------------------------
   // Loops through all the data pulled from API and stores it into individual variables
-  for (i=0; i < all_data.length; i++) { //-----------------------------------------------
+  for (i=0; i < all_data.length; i++) { 
 
     var county_name = all_data[i].county; // COUNTY NAME (ONLY IN CALIFORNIA)
     var wholePop = all_data[i].cvap_net; // ENTIRE POPULATION PER COUNTY
@@ -24,14 +31,19 @@ function loopForData(all_data) {
     var age56_65 = all_data[i].age_56_to_65; // "
     var age66 = all_data[i].age_66_and_over; // "
 
-    // Push the wholePop numbers from each county to two new arrays - " total_pop " & " unsorted_total_pop "
-    total_pop.push(wholePop);
-    unsorted_total_pop.push(wholePop);
-    counties.push(county_name);
-    total_reg.push(total_reg_num);
+    // Push all the data into their own arrays ---------------------------
+    total_pop.push(wholePop); // total population of each county
+    unsorted_total_pop.push(wholePop); // total population of each county (This is a duplicate. Left unsorted for later reference)
+    counties.push(county_name); // county names
+    total_reg.push(total_reg_num); // total registered 
+    group_1.push(age18_25); // number of people registered to vote in age group
+    group_2.push(age26_35); // "
+    group_3.push(age36_45); // "
+    group_4.push(age46_55); // "
+    group_5.push(age56_65); // "
+    group_6.push(age66); // "
 
-
-  } // END OF FOR LOOP --------------------------------------------------------------------
+  } // END OF FOR LOOP -----------------------------------------------------------------------------
 
     // Sort the total population in descending order: Using sort() and compareFunction (for ints)
     // and store in new array
@@ -39,15 +51,23 @@ function loopForData(all_data) {
         return b-a
       });
 
-  // Take counties with highest population 
-  // Loop through array with sorted populations for each county, and save only first 10 (highest)
+  // DECLARATION OF EMPTY ARRAY VARIABLES FOR TOP TEN COUNTIES ----------------
   var topTenPop = [];
   var topTenIndex = [];
   var topTenCounties = [];
   var topTenReg = [];
+  var topTen_Group1 =[];
+  var topTen_Group2 =[];
+  var topTen_Group3 =[];
+  var topTen_Group4 =[];
+  var topTen_Group5 =[];
+  var topTen_Group6 =[];
 
-
+  // BEGIN TOP TEN FOR LOOP ----------------------------------
+  // Take counties with highest population 
+  // Loop through array with sorted populations for each county, and save only first 10 (highest)
   for (j = 0; j < 10; j++) {
+
     // push TOP 10 - TOTAL POPULATION numbers to array
     topTenPop.push(new_total_pop[j]);
 
@@ -60,15 +80,39 @@ function loopForData(all_data) {
     // push TOP 10 - REGISTERED VOTERS numbers to array
     topTenReg.push((total_reg[topTenIndex[j]]))
 
+    // push TOP 10 - GROUP 1 VOTERS numbers to array
+    topTen_Group1.push((group_1[topTenIndex[j]]))
+
+    // push TOP 10 - GROUP 2 VOTERS numbers to array
+    topTen_Group2.push((group_2[topTenIndex[j]]))
+
+    // push TOP 10 - GROUP 3 VOTERS numbers to array
+    topTen_Group3.push((group_3[topTenIndex[j]]))
+
+    // push TOP 10 - GROUP 4 VOTERS numbers to array
+    topTen_Group4.push((group_4[topTenIndex[j]]))
+
+    // push TOP 10 - GROUP 5 VOTERS numbers to array
+    topTen_Group5.push((group_5[topTenIndex[j]]))
+
+    // push TOP 10 - GROUP 6 VOTERS numbers to array
+    topTen_Group6.push((group_6[topTenIndex[j]]))
 
 
 
 
-  };
+
+  }; // END OF TOP TEN FOR LOOP -----------------------------
 
 
   console.log(topTenCounties);
   console.log(topTenReg);
+  console.log(topTen_Group1);
+  console.log(topTen_Group2);
+  console.log(topTen_Group3);
+  console.log(topTen_Group4);
+  console.log(topTen_Group5);
+  console.log(topTen_Group6);
 
 
 
