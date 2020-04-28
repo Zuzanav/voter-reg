dcd3a9ad679749ebfa1ac07cff23e2fecf62141d
Zuzanav <zuzanavolny@gmail.com>
2020-04-28 14:11:07 -0700

    top ten counties retrieved

--- voter-data/app.js
+++ voter-data/app.js
@@ -1,20 +1,19 @@
 $( document ).ready(function() {
 
 
-
 // FUNCTIONS =======================================================================
 
 function loopForData(all_data) {
 
-  console.log(all_data);
-
-  var org_total_pop = [];
+  // EMPTY ARRAYS -------------------
+  var total_pop = [];
   var unsorted_total_pop = [];
+  var counties = [];
 
   // Loops through all the data pulled from API and stores it into individual variables
   for (i=0; i < all_data.length; i++) { //-----------------------------------------------
 
-    var county = all_data[i].county; // COUNTY NAME (ONLY IN CALIFORNIA)
+    var county_name = all_data[i].county; // COUNTY NAME (ONLY IN CALIFORNIA)
     var wholePop = all_data[i].cvap_net; // ENTIRE POPULATION PER COUNTY
     var totalReg = all_data[i].total_registered_voters; // TOTAL REGISTERED TO VOTE PER COUNTY
     var age18_25 = all_data[i].age_18_to_25; // OF AGE GROUP REGISTERED TO VOTE PER COUNTY
@@ -24,43 +23,38 @@ function loopForData(all_data) {
     var age56_65 = all_data[i].age_56_to_65; // "
     var age66 = all_data[i].age_66_and_over; // "
 
-    // Push the wholePop number from each county to new array - " total_pop "
-    org_total_pop.push(wholePop);
+    // Push the wholePop numbers from each county to two new arrays - " total_pop " & " unsorted_total_pop "
+    total_pop.push(wholePop);
     unsorted_total_pop.push(wholePop);
-
-    // PERCENT OF AGE GROUP REGISTERED TO VOTE PER COUNTY
-    let percent_group_1 = (age18_25 / totalReg)*100; 
-    let percent_group_2 = (age26_35 / wholePop)*100;
-    let percent_group_3 = (age36_45 / wholePop)*100;
-    let percent_group_4 = (age46_55 / wholePop)*100;
-    let percent_group_5 = (age56_65 / wholePop)*100;
-    let percent_group_6 = (age66 / wholePop)*100;
-
-    // PERCENT OF POPULATION REGISTERED TO VOTE PER COUNTY
-    //console.log(percent_group_1 + percent_group_2 + percent_group_3 + percent_group_4 + percent_group_5 + percent_group_6);
+    counties.push(county_name);
 
   } // END OF FOR LOOP --------------------------------------------------------------------
 
     // Sort the total population in descending order: Using sort() and compareFunction (for ints)
     // and store in new array
-    var new_total_pop = org_total_pop.sort( function (a, b) {
+    var new_total_pop = total_pop.sort( function (a, b) {
         return b-a
       });
 
-      console.log(unsorted_total_pop);
-
   // Take counties with highest population 
   // Loop through array with sorted populations for each county, and print only first 10 (highest)
-  var topTen = [];
+  var topTenPop = [];
+  var topTenCounties = [];
+
 
   for (j = 0; j < 10; j++) {
-    topTen.push(new_total_pop[j]);
-    console.log( unsorted_total_pop.indexOf(new_total_pop[j]))
+    topTenPop.push(new_total_pop[j]);
+    topTenCounties.push((unsorted_total_pop.indexOf(new_total_pop[j])));
+
+    console.log(counties[topTenCounties[j]]);
+
+
   };
-  console.log(topTen);
 
 
- 
+
+  console.log(topTenCounties);
+
 
 
 
